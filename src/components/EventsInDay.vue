4762ba76aff97bc3f5fff0abed30c4b7b10a38a3
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-08-05 11:47:53 -0500

    rename method

--- src/components/EventsInDay.vue
+++ src/components/EventsInDay.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul v-for="event in this.mapEventName" v-bind:key="event.id" >
+    <ul v-for="event in this.mapEvents" v-bind:key="event.id" >
       <li>
         <a href="#">{{ event.Name }} </a>
       </li>
@@ -24,13 +24,12 @@ export default {
   },
 
   computed: {
-    mapEventName() {
+    mapEvents() {
       if (this.day && this.day.events) {
         let getUniqueEvents = this.day.events.removeDuplicates(this.day.events, "Name")
-        return getUniqueEvents.map(e => e)
-      } else {
-        {}
+        return getUniqueEvents
       }
+      return {}
     }
   },
 
